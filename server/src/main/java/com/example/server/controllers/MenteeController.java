ff6301fcb4c9ebd4dd99434bf71d173507df0dfd
Derek Bracken <derekbracken@gmail.com>
2021-10-19 13:59:20 +0100

    Mentor and Mentee controllers and repos done

--- server/src/main/java/com/example/server/controllers/MenteeController.java
+++ server/src/main/java/com/example/server/controllers/MenteeController.java
@@ -1,4 +1,30 @@
 package com.example.server.controllers;
 
+import com.example.server.models.Mentee;
+import com.example.server.repositories.MenteeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MenteeController {
+
+    @Autowired
+    MenteeRepository menteeRepository;
+
+    @GetMapping(value = "/mentee")
+    public ResponseEntity<Mentee> getMentee(){
+        return new ResponseEntity (menteeRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/mentee")
+    public ResponseEntity<Mentee> postMentee(@RequestBody Mentee mentee){
+        menteeRepository.save(mentee);
+        return new ResponseEntity<>(mentee, HttpStatus.CREATED);
+    }
+
 }
--- server/src/main/java/com/example/server/controllers/MentorController.java
+++ server/src/main/java/com/example/server/controllers/MentorController.java
@@ -1,4 +1,29 @@
 package com.example.server.controllers;
 
+import com.example.server.models.Mentor;
+import com.example.server.repositories.MentorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MentorController {
+
+    @Autowired
+    MentorRepository mentorRepository;
+
+    @GetMapping(value = "/mentor")
+    public ResponseEntity<Mentor> getMentor(){
+        return new ResponseEntity (mentorRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/mentor")
+    public ResponseEntity<Mentor> postMentor(@RequestBody Mentor mentor){
+        mentorRepository.save(mentor);
+        return new ResponseEntity<>(mentor, HttpStatus.CREATED);
+    }
 }
