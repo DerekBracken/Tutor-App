d6db129b29a3e29e44056a94790bcb0656cc4763
HelloMaggieP <m.pentcheva@hotmail.com>
2021-10-20 20:23:31 +0100

    English level repository created

--- server/src/main/java/com/example/server/repositories/AvailabilityRepository.java
+++ server/src/main/java/com/example/server/repositories/AvailabilityRepository.java
@@ -1,7 +1,6 @@
 package com.example.server.repositories;
 
 import com.example.server.models.Availabilty;
-import com.example.server.models.Mentee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvailabilityRepository extends JpaRepository<Availabilty, Long> {
