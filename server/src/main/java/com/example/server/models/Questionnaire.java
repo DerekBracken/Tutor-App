814c175f17f3a400e21730596929e5f40ca3c1b4
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-10-20 15:58:58 +0100

    added logic and commented out

--- server/src/main/java/com/example/server/models/Questionnaire.java
+++ server/src/main/java/com/example/server/models/Questionnaire.java
@@ -2,6 +2,91 @@ package com.example.server.models;
 
 public class Questionnaire {
 
-    
+//    private Mentee mentee;
+//    private ArrayList<Mentor> mentors;
+//
+//    public Preferences(Mentee mentee, ArrayList<Mentor> mentors) {
+//        this.mentee = mentee;
+//        this.mentors = mentors;
+//    }
+//
+//    public Mentee getMentee() {
+//        return mentee;
+//    }
+//
+//    public void setMentee(Mentee mentee) {
+//        this.mentee = mentee;
+//    }
+//
+//    public ArrayList<Mentor> getMentors() {
+//        return mentors;
+//    }
+//
+//    public void setMentors(ArrayList<Mentor> mentors) {
+//        this.mentors = mentors;
+//    }
+//
+//    public void locationScore(){
+//        for (Mentor mentor : mentors) {
+//            if (mentor.getLocation().equals(mentee.getLocation())) {
+//                mentor.increaseSuitabilityScore(mentee.getLocationWeighting());
+//            }
+//        }
+//    }
+//
+//    public void teachingScore(){
+//        for (Mentor mentor : mentors){
+//            if (mentor.getCapability().contains(mentee.getEnglishLevel())){
+//                mentor.increaseSuitabilityScore(5);
+//            }
+//        }
+//    }
+//
+//    public void languagesScore(){
+//        for (Mentor mentor : mentors){
+//            if (mentor.getSpokenLanguages().contains(mentee.getNativeLanguage())){
+//                mentor.increaseSuitabilityScore(mentee.getLanguagesWeighting());
+//            }
+//        }
+//    }
+//
+//    public void genderScore(){
+//        for (Mentor mentor: mentors) {
+//            if (mentor.getGender().equals(mentee.getGenderPref()) || mentee.getGenderPref().equals("No preference")){
+//                mentor.increaseSuitabilityScore(mentee.getGenderWeight());
+//            }
+//        }
+//    }
+//
+////    public void ageScore(){
+////        to check the age comparison
+////
+////    }
+//
+//    public void availabilityScore(){
+//        for (int i=0; i<mentee.getAvailability().size(); i++){
+//            for (Mentor mentor: mentors){
+//                if (mentor.getAvailability().contains(mentee.getAvailability().get(i))){
+//                    mentor.increaseSuitabilityScore(5);
+//                }
+//            }
+//        }
+//    }
+//
+//    public void getTotalScores(){
+//        locationScore();
+//        teachingScore();
+//        languagesScore();
+//        genderScore();
+////        ageScore();
+//        availabilityScore();
+//    }
+//
+////    public void sortMentors(){
+////        for (Mentor mentor: mentors){
+////
+////        }
+////    }
+//}
 
 }
