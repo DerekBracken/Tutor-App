91f29996b49de3138836eb85d4736c721228ff12
DSoutter <duncan.soutter@gmail.com>
2021-10-21 11:18:49 +0100

    getting database on front end

--- client/src/containers/AllMentorsContainer.js
+++ client/src/containers/AllMentorsContainer.js
@@ -1,8 +1,22 @@
 import HeaderComponent from "../components/HeaderComponent";
 import MatchedMentorList from "../components/MatchedMentorList";
 import { withRouter } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 const AllMentorsContainer = () => {
+
+    const [allMentors, setAllMentors] = useState(null);
+
+    useEffect(() => {
+        getAllMentors();
+    },[])
+
+    const getAllMentors = function() {
+        fetch("http://localhost:8080/mentors")
+        .then(res => res.json())
+        .then(allMentors => setAllMentors(allMentors))
+        .then(console.log(allMentors))
+    }
 
     return(
         <>
