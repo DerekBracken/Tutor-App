038e21804fcce676c1acf6f47979d75228049a71
Derek Bracken <derekbracken@gmail.com>
2021-10-24 18:27:26 +0100

    Renamed postMentor method and all references to post

--- client/src/components/PersonalInformation.js
+++ client/src/components/PersonalInformation.js
@@ -9,20 +9,22 @@ const PersonalInformation = () => {
         getUser();
     },[])
 
+    // Will need to add fetch for mentees too, and add a enum type to mentor/mentee to control flow.
+    // USe a try catch for both fetch statements
     const getUser = function() {
-        fetch(`http://localhost:8080/mentees?email=${currentUser.email}`)
+        fetch(`http://localhost:8080/mentors?email=${currentUser.email}`)
         .then(res => res.json())
         .then(user => setUser(user))
     }
     if (user != null){
-      console.log(user[0].firstName);
-      console.log(user[0].mentor.id);
+      console.log(user);
     } 
 
     return (
         <div>
           {currentUser && currentUser.email} Has Signed In
-          {user && user[0].firstName} Is my name 
+          <br/>
+          {user && user.firstName} Is my name 
         </div>
     );
   }
