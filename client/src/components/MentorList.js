d58d789a705c9854b28b8f601a9ea7e7b882930d
DSoutter <duncan.soutter@gmail.com>
2021-10-26 15:49:46 +0100

    sort of working logic for getTotalScores method

--- client/src/components/MentorList.js
+++ client/src/components/MentorList.js
@@ -2,7 +2,7 @@ import Mentor from "./Mentor";
 import { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import Service from "../services/service";
-
+import getTotalScores from "../models/Matchmaking";
 
 
 const MentorList = ({allMentors}) => {
@@ -43,9 +43,14 @@ const MentorList = ({allMentors}) => {
             </>
         )
     })
-   
+   if (user === null){
+       return <h1>hello</h1>
+   }
     const chooseMentor = allMentors.map((mentor, index) => {
 
+        getTotalScores(user, allMentors, 5,5,5,5,5)
+        console.log("This is the mentee: ", user)
+        console.log("This is the mentors: ", allMentors)
         return (
             <div>
                 <Mentor mentor = {mentor} key = {index}/>
--- client/src/models/Matchmaking.js
+++ client/src/models/Matchmaking.js
@@ -1,25 +1,22 @@
 
-// const dict = {};
+const dict = {};
 
 function dictionarySetup(mentorArray){
-    for (let i=0; i<mentorArray.length(); i++ ){
-        dict.push({
-            key: mentorArray[i],
-            value = 0
-        })
+    for (let i=0; i<mentorArray.length; i++){
+        dict[mentorArray[i]] = 0
+        }
     }
-}
 
 function locationScore(mentee, mentorArray, locationWeighting){
-    for (let i=0; i<mentorArray.length(); i++ ){
-        if (mentorArray[i].location === mentee.location){
+    for (let i=0; i<mentorArray.length; i++ ){
+        if (mentorArray[i].location == mentee.location){
             dict[mentorArray[i]] += locationWeighting
         }
     }
 }
 
 function teachingScore(mentee, mentorArray, teachingWeighting){
-    for (let i=0; i<mentorArray.length(); i++ ){
+    for (let i=0; i<mentorArray.length; i++ ){
         if (mentorArray[i].teachingLevel.includes(mentee.englishLevel)){
             dict[mentorArray[i]] += teachingWeighting
         }
@@ -27,9 +24,9 @@ function teachingScore(mentee, mentorArray, teachingWeighting){
 }
 
 function languagesScore(mentee, mentorArray, languagesWeighting){
-    for (let i=0; i<mentorArray.length(); i++ ){
-        for (let j=0; j<mentee.languagesSpoken.length(); j++ ){
-            if (mentorArray[i].languagesSpoken.contains(mentee.languagesSpoken[j])){
+    for (let i=0; i<mentorArray.length; i++ ){
+        for (let j=0; j<mentee.languagesSpoken.length; j++ ){
+            if (mentorArray[i].languagesSpoken.includes(mentee.languagesSpoken[j])){
                 dict[mentorArray[i]] += languagesWeighting
             }
         }
@@ -37,7 +34,7 @@ function languagesScore(mentee, mentorArray, languagesWeighting){
 }
 
 function genderScore(mentee, mentorArray, genderWeighting){
-    for (let i=0; i<mentorArray.length(); i++ ){
+    for (let i=0; i<mentorArray.length; i++ ){
         if (mentorArray[i].gender === mentee.genderPref || mentee.genderPref === "No preference"){
             dict[mentorArray[i]] += genderWeighting
         }
@@ -45,16 +42,16 @@ function genderScore(mentee, mentorArray, genderWeighting){
 }
 
 function availabilityScore(mentee, mentorArray, availabilityWeighting){
-    for (let i=0; i<mentorArray.length(); i++ ){
-        for (let j=0; j<mentee.availability.length(); j++ ){
-            if (mentorArray[i].availability.contains(mentee.availability[j])){
-                dict[mentorArray[i] += availabilityWeighting]
+    for (let i=0; i<mentorArray.length; i++ ){
+        for (let j=0; j<mentee.availability.length; j++ ){
+            if (mentorArray[i].availability.includes(mentee.availability[j])){
+                dict[mentorArray[i]] += availabilityWeighting
             }
         }
     }
 }
 
-function getTotalScores(mentee, mentorArray, locationWeighting, teachingWeighting, languagesWeighting, genderWeighting, availabilityWeighting){
+export function getTotalScores(mentee, mentorArray, locationWeighting, teachingWeighting, languagesWeighting, genderWeighting, availabilityWeighting){
 
 dictionarySetup(mentorArray);
 locationScore(mentee, mentorArray, locationWeighting);
