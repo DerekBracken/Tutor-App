ad54ffb5a54258bcecbf7c2d6d0a0ded3c7ceaba
HelloMaggieP <m.pentcheva@hotmail.com>
2021-10-26 22:06:28 +0100

    mentor display generally done

--- client/src/components/Mentor.js
+++ client/src/components/Mentor.js
@@ -1,10 +1,32 @@
-const Mentor = ({mentor}) => {
+import "../styles/mentor.css"
 
-    
+const Mentor = ({mentor}) => {
 
     return(
         <>
-        <p>Name: {mentor.firstName} {mentor.lastName}, Email: {mentor.email}, Reason for joining service: {mentor.motivation}, Location: {mentor.location}, Gender: {mentor.gender} </p>
+        <div id="individual-mentor-container">
+            <h4 id="name" > {mentor.firstName} {mentor.lastName}</h4>
+            <h4 id="motivation"> "{mentor.motivation}"</h4>
+                <div id="optional-info">
+                    <h4 className="mentor-tags">Location</h4>  <h4 className="mentor-info"> {mentor.location}</h4> 
+                </div>
+                <div id="optional-info">
+                    <h4 className="mentor-tags">Gender </h4> <h4 className="mentor-info"> {mentor.gender} </h4>
+                    </div>
+                    <div id="optional-info">
+                    <h4 className="mentor-tags">Spoken Languages </h4> <h4 className="mentor-info" > {mentor.languagesSpoken}</h4>
+                    </div>
+                    <div id="optional-info">
+                    </div>
+                    <div id="optional-info">
+                    <h4 className="mentor-tags">Teaching Level</h4> <h4 className="mentor-info"> {mentor.teachingLevel} </h4>
+                    </div>
+                    <div id="optional-info">
+                    <h4 className="mentor-tags">Availability</h4> <h4 className="mentor-info" >{mentor.availability}</h4>
+                    </div>
+               
+        </div>
+        
         </>
     )
 }
