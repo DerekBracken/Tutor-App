3062f7f2603bcba891dfc303ef865e0f27d8af6e
HelloMaggieP <m.pentcheva@hotmail.com>
2021-10-27 11:24:18 +0100

    mentee sign up form done

--- client/src/components/MenteeSignupFormComponent.js
+++ client/src/components/MenteeSignupFormComponent.js
@@ -42,24 +42,34 @@ const MenteeSignupFormComponent = () =>{
     return (
         <>
             <h1 id="signup-form-heading" >Mentee Signup Form Component</h1>
-            <form onSubmit={handleFormSubmit} id="mentee-form-container">
+            <form onSubmit={handleFormSubmit} id="mentor-form-container">
+
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="first_name">First name: </label><br/>
-                    <input className="form-field" type='text' id='first-name' name="first_name" placeholder="Enter First Name" required/> <br/>
-                
-                <label className="mentee-form-label" HTMLfor="last_name">Last name:</label><br/>
+                <input className="form-field" type='text' id='first-name' name="first_name" placeholder="Enter First Name" required/> <br/>
+            </div>
+
+            <div className="form-field-container">
+                    <label className="mentee-form-label" HTMLfor="last_name">Last name:</label><br/>
                     <input className="form-field" type='text' id='last-name' name="last_name" placeholder="Enter Last Name" required/><br/>
-                
+            </div>     
+                    
+        <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="dob"> Enter date of birth: </label><br/>
                     <input className="form-field" type="date" id='dob' name='dob' required/><br/>
-                
+        </div>
                 {/* <label>
                 will prepopulate with email from Dereks bit
                     Enter email address: 
                     <input type='email' id='email' name='email' placeholder="Enter email address" required/>
                 </label> */}
+
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="contact_number">Enter contact number: </label><br/>
                     <input className="form-field" type='tel' id='contact-number' name='contact_number' placeholder="07856 658 658" required/><br/>
-                
+            </div>
+
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="gender">Gender identity: </label><br/>
                     <select className="form-field" name='gender' id='gender' required>
                         <option disabled selected>Select from below</option>
@@ -67,10 +77,14 @@ const MenteeSignupFormComponent = () =>{
                             <option value="female">Female</option>
                             <option value="other">Other</option>
                     </select>
-                
+            </div>
+
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="about_me">About you: </label><br/>
                     <textarea className="form-field" id='about_me' name='about_me' placeholder='I am from....' required/><br/>
+            </div>
 
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="location"> Where are you based? </label><br/>
                     <select className="form-field" name='location' id='location' required><br/>
                     <option disabled selected>Select from below</option>
@@ -81,8 +95,9 @@ const MenteeSignupFormComponent = () =>{
                         <option value="Inverness">Inverness</option>
                         <option value="Stirling">Stirling</option>
                     </select>
-                
+            </div>
 
+            <div className="form-field-container">
                 <label className="mentee-form-label" >What English level are you?</label><br/>
                     <select className="form-field" name='english_level' id='english_level' required>
                         <option disabled selected>Select</option>
@@ -93,23 +108,34 @@ const MenteeSignupFormComponent = () =>{
                             <option value="upper_intermediate">Upper-Intermediate</option>
                             <option value="advanced">Advanced</option>
                     </select>
+            </div>
 
+            <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="languages_spoken">What languages to do you speak? </label><br/>
                     <input className="form-field" type='text' id='languages_spoken' name="languages_spoken" placeholder="e.g. Polish" required/><br/>
                         <div>
                             <label className="mentee-form-label" > Select availability: </label>
-                            <label className="mentee-form-small-options" for="mon_fri_day">Monday-Friday Daytime</label>
+                            <div>
                             <input type="checkbox" name="mon_fri_day" value="mon_fri_day" id="mon_fri_day"/>
-                                                                       
-                            <label className="mentee-form-small-options" for="mon_fri_eve">Monday-Friday Evening</label>
+                            <label className="mentee-form-small-options" for="mon_fri_day">Monday-Friday Daytime</label>
+                            </div>
+
+                            <div>
                             <input type="checkbox" name="mon_fri_eve" value="mon_fri_eve" id="mon_fri_eve"/>
-                                                                    
-                            <label className="mentee-form-small-options" for="weekend_day">Weekend Daytime</label>
+                            <label className="mentee-form-small-options" for="mon_fri_eve">Monday-Friday Evening</label>
+                            </div>
+
+                             <div>                                       
                             <input type="checkbox" name="weekend_day" value="weekend_day" id="weekend_day"/>
-                                                                    
-                            <label className="mentee-form-small-options" for="weekend_eve">Weekend Evening</label>
-                            <input type="checkbox" name="weekend_eve" value="weekend_eve" id="weekend_eve"/>                                   
+                            <label className="mentee-form-small-options" for="weekend_day">Weekend Daytime</label>
+                            </div>
+
+                             <div>                                       
+                            <input type="checkbox" name="weekend_eve" value="weekend_eve" id="weekend_eve"/> 
+                            <label className="mentee-form-small-options" for="weekend_eve">Weekend Evening</label>   
+                            </div>                               
                         </div>
+                </div>
                 <input className="small-button" type='submit' value='Submit'/>
             </form>
         </>
