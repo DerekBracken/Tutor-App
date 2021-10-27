0e4a5bd2111fe6b9aa667717d3a7c76adfe11301
HelloMaggieP <m.pentcheva@hotmail.com>
2021-10-27 23:09:59 +0100

    how it works page filled out

--- client/src/containers/HowItWorksContainer.js
+++ client/src/containers/HowItWorksContainer.js
@@ -10,8 +10,18 @@ const HowItWorksContainer = ({user}) => {
         <HeaderComponent user={user}/>
 
        <h1>How MeetEng Works</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
+       <h2 className="how-it-works-steps">Step 1 </h2>
+        <p className="how-it-works">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
 
+        <h2 className="how-it-works-steps">Step 2 </h2>
+        <p className="how-it-works">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
+        
+        <h2 className="how-it-works-steps">Step 3 </h2>
+        <p className="how-it-works">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
+        
+        <h2 className="how-it-works-steps">Step 4 </h2>
+        <p className="how-it-works">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
+        
         <Footer/>
         </>
     )
