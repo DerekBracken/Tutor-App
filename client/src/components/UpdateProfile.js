228834f28abe2a58b7fc12392612573e46225862
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-10-27 16:35:04 +0100

    finished formatting update form

--- client/src/components/UpdateProfile.js
+++ client/src/components/UpdateProfile.js
@@ -1,27 +1,37 @@
+import HeaderComponent from "./HeaderComponent";
 
 const UpdateProfile = ({user}) => {
 
     if ( user != null){
     return (
         <>
-            <form id="mentor-form-container">
+
+            <HeaderComponent/>
 
             <h1 id="signup-form-heading" >Update your profile</h1>
+
+            <form id="user-form-container">
 
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="first_name">First name: </label>
                 <input className="form-field" type='text' id='first-name' name="first_name" placeholder={user.firstName}/>
             </div>
 
+            <br/>
+
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="last_name">Last name:</label>
                 <input className="form-field" type='text' id='last-name' name="last_name" placeholder={user.lastName}/>
             </div>
 
+            <br/>
+
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="dob"> Enter date of birth: </label>
                 <input className="form-field" type="date" id='dob' name='dob' placeholder={user.dateOfBirth}/>
             </div>
+
+            <br/>
 
             {/* EMAIL ADDRESS */}
             
@@ -29,6 +39,8 @@ const UpdateProfile = ({user}) => {
                 <label className="mentee-form-label" HTMLfor="contact_number">Enter contact number: </label>
                 <input className="form-field" type='tel' id='contact-number' name='contact_number' placeholder={user.contactNumber}/>
             </div>
+
+            <br/>
 
             <div className="form-field-container">
             <label className="mentee-form-label" HTMLfor="gender">Gender identity: </label>
@@ -40,10 +52,14 @@ const UpdateProfile = ({user}) => {
             </select>
             </div>
 
+            <br/>
+
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="about_me">About you: </label>
                 <textarea className="form-field" id='about_me' name='about_me' placeholder={user.aboutMe}/>
             </div>
+
+            <br/>
 
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="location"> Where are you based? </label>
@@ -58,6 +74,8 @@ const UpdateProfile = ({user}) => {
                 </select>
             </div>
 
+            <br/>
+
             <div className="form-field-container">
             <label className="mentee-form-label">What English level are you?</label>
                     <select className="form-field" name='english_level' id='english_level' required>
@@ -71,6 +89,8 @@ const UpdateProfile = ({user}) => {
                     </select>
             </div>
 
+            <br/>
+
             <div className="form-field-container">
                 <label className="mentee-form-label" HTMLfor="languages_spoken">What languages to do you speak? </label>
                 <input className="form-field" type='text' id='languages_spoken' name="languages_spoken" placeholder={user.languagesSpoken}/>
@@ -80,25 +100,27 @@ const UpdateProfile = ({user}) => {
                         <label className="mentee-form-label"> Select availability: </label>
 
                         <div>
-                            <label className="mentee-form-small-options" for="mon_fri_day">Monday-Friday Daytime</label>
                             <input type="checkbox" name="mon_fri_day" value="mon_fri_day" id="mon_fri_day"/>
+                            <label className="mentee-form-small-options" for="mon_fri_day">Monday-Friday Daytime</label>
                         </div>
 
                         <div>                                  
-                            <label className="mentee-form-small-options" for="mon_fri_eve">Monday-Friday Evening</label>
                             <input type="checkbox" name="mon_fri_eve" value="mon_fri_eve" id="mon_fri_eve"/>
+                            <label className="mentee-form-small-options" for="mon_fri_eve">Monday-Friday Evening</label>
                         </div>
 
                         <div>                                      
-                            <label className="mentee-form-small-options" for="weekend_day">Weekend Daytime</label>
                             <input type="checkbox" name="weekend_day" value="weekend_day" id="weekend_day"/>
+                            <label className="mentee-form-small-options" for="weekend_day">Weekend Daytime</label>
                         </div>
 
                         <div>                                        
-                            <label className="mentee-form-small-options" for="weekend_eve">Weekend Evening</label>
                             <input type="checkbox" name="weekend_eve" value="weekend_eve" id="weekend_eve"/>  
+                            <label className="mentee-form-small-options" for="weekend_eve">Weekend Evening</label>
                         </div>                    
                     </div>
+
+                    <br/>
 
             <input className="small-button" type='submit' value='Submit'/>
 
