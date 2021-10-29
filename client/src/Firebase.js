01b571cca7e11fd196f1ddc5a25c848d9b1235fa
Derek Bracken <derekbracken@gmail.com>
2021-10-29 16:38:47 +0100

    Added firebase config from .env.local to firebase.js

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -2,12 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCX4X0oql5b7P-ZQR_1N5KIUak_WrXjbrQ",
+    authDomain: "meeteng-8919f.firebaseapp.com",
+    databaseURL: "https://meeteng-8919f-default-rtdb.firebaseio.com",
+    projectId: "meeteng-8919f",
+    storageBucket: "meeteng-8919f.appspot.com",
+    messagingSenderId: "203506631641",
+    appId: "1:203506631641:web:52579785e86bf6a9468fc8",
+    measurementId: "G-N4X78FYCZB"
 })
 
 export const auth = app.auth()
